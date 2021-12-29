98ca3d6fb89771e4e8920b85c9d945f5e3e8775e
AllWillBePerfect <reserveproject@mail.ru>
2021-12-29 19:40:12 +0300

    domain is ready

--- app/src/main/java/com/example/shoppinglsit/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglsit/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.shoppinglsit
+package com.example.shoppinglsit.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.shoppinglsit.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
